155cc9415fa39afdbc2bfc314133a1dfb5ba1c84
Jean Lucas <jean@4ray.co>
2019-05-19 16:23:27 -0400

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # tenshi
-Unbound local-data generator for curated hosts files
+
+This utility fetches a curated hosts file, generates Unbound local-data from it, and saves the result in `/etc/unbound/local-blocking-data.conf`.
+
+At the moment, only Steven Black's hosts file is used. Source: https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts.
+
+This utility is meant to work on Linux. It is untested on Mac, and probably will not work on Windows.
+
+You will want to add `include: "local-blocking-data.conf"` to your Unbound config to use the result.
+
+This package is available on crates.io at https://crates.io/crates/tenshi, and the Arch User Respository at https://aur.archlinux.org/packages/tenshi.
+
+## Installation
+
+### Prerequisite
+
+Install either [rustup](http://rustup.rs/) or `rust` using your favorite package manager.
+
+This utility has been tested to work with Rust 1.34.2.
+
+`sudo` is needed if you do not have write access to `/etc/unbound/local-blocking-data.conf`.
+
+### Build with Cargo
+
+`cargo install tenshi`
+
+### Arch Linux
+
+```
+git clone https://aur.archlinux.org/tenshi.git
+cd tenshi
+makepkg -i
+```
+
+### Build from source
+
+```
+git clone https://github.com/flacks/tenshi.git
+git checkout $(git tag | tail -n 1) # Optional
+cd tenshi
+cargo build --release --locked
+cargo install --path .
+```
+
+## Synopsis
+
+`sudo tenshi`, excluding `sudo` if you have write access to `/etc/unbound/local-blocking-data.conf`.
+
+Utility will prompt to continue. For promptless execution, run `echo y | sudo tenshi`.
+
+## To do
+
+- [ ] *Actually* support different hosts files
+  * Upstream variants
+- [ ] Custom hosts files support
+- [ ] Argument support
+- [ ] Quiet mode
+- [ ] Support changing data destination
+- [ ] Modularize code
+
+## Credits
+
+Inspired by https://github.com/gbxyz/unbound-block-hosts
+
+A kind Rust Discord server user on #beginners
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::error::Error;
+use std::io::{stdin, stdout, BufWriter, Write};
+
+fn main() {
+    println!("tenshi 0.1.0 :: Jean Lucas <jean@4ray.co>\n");
+
+    println!("This utility will fetch Steven Black's adware + malware hosts file and generate Unbound local-data from it\n");
+
+    println!("Data will be saved to /etc/unbound/local-blocking-data.conf");
+    println!("You must have permission to write to that file\n");
+
+    println!(
+        "Add `include: \"local-blocking-data.conf\"` to your Unbound config to use the result\n"
+    );
+
+    // Prompt to continue
+    loop {
+        print!("Enter \"y\" to continue, \"q\" to quit: ");
+        stdout().flush().unwrap();
+
+        let mut confirm = String::new();
+        stdin()
+            .read_line(&mut confirm)
+            .expect("Failed to read line");
+        match confirm.trim() {
+            "y" => {
+                println!("");
+                break;
+            }
+            "q" => std::process::exit(0),
+            _ => println!("\nInvalid response\n"),
+        }
+    }
+
+    // Check write permission
+    print!("Checking write permission... ");
+    stdout().flush().unwrap();
+    let path = std::path::Path::new("/etc/unbound/local-blocking-data.conf");
+    let display = path.display();
+    let file = match std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&path)
+    {
+        Err(why) => panic!("Couldn't write to {}: {}", display, why.description()),
+        Ok(file) => {
+            println!("Success");
+            file
+        }
+    };
+
+    // Fetch hosts file
+    let client = reqwest::Client::new();
+    print!("Fetching hosts file... ");
+    stdout().flush().unwrap();
+    let mut request = client
+        .get("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
+        .send()
+        .unwrap();
+    println!("Success");
+    let hosts = request.text().unwrap();
+
+    // Gather results using regex and write local-data entries to file buffer
+    let re = regex::Regex::new(r"(?m)^0.0.0.0 (\S+)").unwrap();
+    let mut hosts_buf = BufWriter::new(file);
+    print!("Generating local-data... ");
+    stdout().flush().unwrap();
+    for cap in re.captures_iter(&hosts) {
+        write!(
+            &mut hosts_buf,
+            "local-data: \"{} A 0.0.0.0\"\nlocal-data: \"{} AAAA ::\"\n",
+            &cap[1], &cap[1]
+        )
+        .unwrap();
+    }
+
+    println!("Success")
+}
